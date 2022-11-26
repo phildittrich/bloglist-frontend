426cf8eeeb722b265c272a652a9b3ba77b17d415
Philipp Dittrich <mail@philippdittri.ch>
2022-11-26 19:13:14 +0300

    ex 5.9

--- src/App.js
+++ src/App.js
@@ -131,9 +131,10 @@ const App = () => {
         <CreateBlog createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} update={updateBlog}/>
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes )
+        .map(blog => <Blog key={blog.id} blog={blog} update={updateBlog}/>)
+      }
     </div>
   )
 }
