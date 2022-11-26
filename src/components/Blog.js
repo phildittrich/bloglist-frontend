937a5ebb8e568749d5d25e2099b35df3a3d67308
Philipp Dittrich <mail@philippdittri.ch>
2022-11-26 15:07:46 +0300

    ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+    console.log(blog)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleVisibility}>
+          {visible ?
+            'hide' :
+            'show' 
+          }
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        {blog.likes}<br />
+        {blog.user && blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
